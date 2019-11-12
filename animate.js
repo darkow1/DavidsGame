f9ee51520671fc032c7f758f6cc56a49c6876c8b
Stephanie <ChoFamilia@Hannys-MacBook-Air.local>
2019-11-11 20:26:17 -0800

    moving paddle and falling object

--- animate.js
+++ animate.js
@@ -2,16 +2,20 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 50;
-var paddleWidth = 50;
+var dx = 1;
+var dy = -1;
+var paddleHeight = 60;
+var paddleWidth = 10;
 var paddleX = (canvas.width-paddleWidth)/2;
-var paddleY = (canvas.height-paddleHeight)/2;
+var paddleY = canvas.height - 60;
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
+var objectHeight = 30;
+var objectWidth = 30;
+var objectX = (canvas.width-Math.random()*500)/2;
+var objectY = 0;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
@@ -49,35 +53,53 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
+
+function drawObject(){
+    ctx.beginPath();
+    ctx.rect(objectX, objectY, objectHeight, objectWidth);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    moveObject();
+}
+
+function moveObject() {
+  objectY+=5;
+  if (paddleY < 0){
+      paddleY = 0;
+}
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPaddle();
+    drawObject();
     if(rightPressed) {
-        paddleX += 2;
+        paddleX += 7;
         if (paddleX + paddleWidth > canvas.width){
             paddleX = canvas.width - paddleWidth;
         }
     }
     else if(leftPressed) {
-        paddleX -= 2;
+        paddleX -= 7;
         if (paddleX < 0){
             paddleX = 0;
         }
     }
     else if(upPressed) {
-        paddleY -= 2;
+        paddleY -= 7;
         if (paddleY < 0){
             paddleY = 0;
         }
 
     }
     else if(downPressed) {
-        paddleY += 2;
+        paddleY += 7;
         if (paddleY + paddleHeight > canvas.height){
             paddleY = canvas.height - paddleHeight;
         }
     }
     x += dx;
     y += dy;
+
 }
-setInterval(draw, 1);
+setInterval(draw, 20);
